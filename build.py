dee31cc3de7abf025c4fc3c6737488fe04e941ec
Matthew <111226411+Lagiacrus96@users.noreply.github.com>
2024-05-05 12:39:53 +0200

    Update build.py
    Added more linting fixes.

--- build.py
+++ build.py
@@ -7,17 +7,18 @@ def copy_file(source_path, destination_dir, file_name):
     """Copies a file from source_path to destination_dir."""
     # Make sure the destination directory exists
     os.makedirs(destination_dir, exist_ok=True)
-    
+
     # Full path for the destination file
     dest_file_path = os.path.join(destination_dir, file_name)
-    
+
     # Copy the file
     shutil.copyfile(source_path, dest_file_path)
-    
+
     # Success
     print('Success :)!')
 
 def main():
+    """Executes the file copying process for deployment."""
     source_file_path = 'index.html'
     dest_directory = 'dist'
     file_name = 'index.html'
