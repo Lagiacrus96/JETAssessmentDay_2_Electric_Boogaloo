789bb09ccef4693a35ebebd8dd1a5a9cc13a4607
Matthew <mattmeinema@hotmail.com>
2024-05-07 11:39:56 +0200

    Added Terraform files to create the storage account/s3 bucket to host the static websites.

---
+++ aws/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+    profile = "default"
+    region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "jet_website_bucket" {
+    bucket = "jet-website-bucket"
+}
+
+resource "aws_s3_bucket_website_configuration" "config" {
+    bucket = aws_s3_bucket.jet_website_bucket.bucket
+    index_document {
+        suffix = "index.html"
+    }
+    error_document {
+        key = "404.html"
+    }
+}
