b6447247970098172747da0aabec6763b632111a
Matthew <111226411+Lagiacrus96@users.noreply.github.com>
2024-05-13 14:20:02 +0200

    Update main.tf
    Applying environment variables to Azure main.tf

--- azure/main.tf
+++ azure/main.tf
@@ -1,7 +1,12 @@
 # Define the provider
 provider "azurerm" {
-    features {}
+  features {}
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
 }
+
 
 # Create a resource group
 resource "azurerm_resource_group" "rg" {
